24ca07f294a7840f779246deae87da2ab1f8b1c4
zhenyuWang <13641039885@163.com>
2023-09-27 10:52:55 +0800

    perf: no comment prompted

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -5,14 +5,18 @@ function CommentList(props) {
 
   return (
     <ul className={classes.comments}>
-      {items.map((item) => (
-        <li key={item._id}>
-          <p>{item.text}</p>
-          <div>
-            By <address>{item.name}</address>
-          </div>
-        </li>
-      ))}
+      {items.length ? (
+        items.map((item) => (
+          <li key={item._id}>
+            <p>{item.text}</p>
+            <div>
+              By <address>{item.name}</address>
+            </div>
+          </li>
+        ))
+      ) : (
+        <p className='font-size-14'>No comment yet.</p>
+      )}
     </ul>
   )
 }
